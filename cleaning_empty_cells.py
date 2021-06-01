49ea4035d3a95f1dba56f9e8a8600c4967044378
parth2002g <72256367+parth2002g@users.noreply.github.com>
2021-06-01 20:46:16 +0530

    Update cleaning_empty_cells.py

--- cleaning_empty_cells.py
+++ cleaning_empty_cells.py
@@ -4,35 +4,33 @@ df = pd.read_csv("dirtydata.csv")
 
 print(df.to_string())
 
-"""
+
 df.dropna(inplace = True)
 
 print(df.to_string())
 
-"""
-"""
+
 df.fillna(130, inplace = True)
 
 print(df.to_string())
-"""
-"""
+
+
 df["Calories"].fillna(130, inplace = True)
 print(df.to_string())
-"""
-"""
+
+
 x = df["Calories"].mean()
 
 df["Calories"].fillna(x, inplace = True)
 
 print(df.to_string())
-"""
-"""
+
+
 x = df["Calories"].median()
 
 df["Calories"].fillna(x, inplace = True)
 
 print(df.to_string())
-"""
 
 x = df["Calories"].mode()[0]
 
